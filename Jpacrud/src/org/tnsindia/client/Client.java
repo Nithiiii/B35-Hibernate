1122d18f82bbc6233220fc7c90f0e39002eea619
Nithe <Nithe@scorpion>
2023-02-01 15:57:08 +0530

    first commit

---
+++ Jpacrud/src/org/tnsindia/client/Client.java
@@ -0,0 +1,40 @@
+package org.tnsindia.client;
+
+import org.tnsindia.entities.Student;
+import org.tnsindia.service.StudentService;
+import org.tnsindia.service.StudentServiceImpl;
+
+public class Client {
+
+	public static void main(String[] args) {
+	
+		StudentService service=new StudentServiceImpl();
+		
+		Student student=new Student();
+		
+		//creation
+		
+		student.setRollNo(123);
+		student.setName("nitheen");
+		service.create(student);
+		
+		System.out.println("The data is inserted");
+		
+		//Retrieve
+		student=service.retrieve(1213);
+		System.out.println("ROLL NO: "+student.getRollNo()+" "+"Name: "+
+		student.getName());
+	
+		//update
+		student=service.retrieve(1213);
+		student.setName("Harshitha Muniraju");
+		service.update(student);
+		System.out.println("The data is updated");
+		
+		//delete
+		student=service.retrieve(1214);
+		service.delete(student);
+		System.out.println("The data is deleted");
+	}
+
+}
